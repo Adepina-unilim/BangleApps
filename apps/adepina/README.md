3198f9c674b556b4d3fa4bc09e3ac321c0830387
Adepina-unilim <107838807+Adepina-unilim@users.noreply.github.com>
2022-06-20 10:13:38 +0200

    Add files via upload

---
+++ apps/adepina/README.md
@@ -0,0 +1,11 @@
+# Adepina
+
+Enregistre les données issues du magnétomètre et de l'accéléromètre (à la fréquence d'échantillonnage de 12,5 Hz) dans un fichier CSV.
+
+
+## Comment accéder aux données depuis un ordinateur ?
+
+* Aller sur l'App Loader d'Adepina
+* Se connecter à la montre via Bluetooth
+* Dans l'onglet `My Apps`, cliquer sur l'application `Adepina`
+* Vous pouvez alors enregistrer ou supprimer les données :)
---
+++ apps/adepina/app.js
@@ -0,0 +1,142 @@
+var fileNumber = 0;
+var MAXLOGS = 9;
+
+function getFileName(n) {
+  return "accellog."+n+".csv";
+}
+
+function showMenu() {
+  var menu = {
+    "" : { title : /*LANG*/"Adepina" },
+    "< Retour" : function() {
+      load();
+    },
+    /*LANG*/"Fichier Num" : {
+      value : fileNumber,
+      min : 0,
+      max : MAXLOGS,
+      onchange : v => { fileNumber=v; }
+    },
+    /*LANG*/"Commencer" : function() {
+      E.showMenu();
+      startRecord();
+    },
+    /*LANG*/"Voir les données" : function() {
+      viewLogs();
+    },
+  };
+  E.showMenu(menu);
+}
+
+function viewLog(n) {
+  E.showMessage(/*LANG*/"Loading...");
+  var f = require("Storage").open(getFileName(n), "r");
+  var records = 0, l = "", ll="";
+  while ((l=f.readLine())!==undefined) {records++;ll=l;}
+  var length = 0;
+  if (ll) length = Math.round( (ll.split(",")[0]|0)/1000 );
+
+  var menu = {
+    "" : { title : "Log "+n },
+    "< Retour" : () => { viewLogs(); }
+  };
+  menu[records+/*LANG*/" Valeurs"] = "";
+  menu[length+/*LANG*/" Secondes"] = "";
+  menu[/*LANG*/"DELETE"] = function() {
+    E.showPrompt(/*LANG*/"Supprimer les données "+n).then(ok=>{
+      if (ok) {
+        E.showMessage(/*LANG*/"Supression...");
+        f.erase();
+        viewLogs();
+      } else viewLog(n);
+    });
+  };
+
+
+  E.showMenu(menu);
+}
+
+function viewLogs() {
+  var menu = {
+    "" : { title : /*LANG*/"Logs" },
+    "< Retour" : () => { showMenu(); }
+  };
+
+  var hadLogs = false;
+  for (var i=0;i<=MAXLOGS;i++) {
+    var f = require("Storage").open(getFileName(i), "r");
+    if (f.readLine()!==undefined) {
+      (function(i) {
+        menu[/*LANG*/"Log "+i] = () => viewLog(i);
+      })(i);
+      hadLogs = true;
+    }
+  }
+  if (!hadLogs)
+    menu[/*LANG*/"Pas de données trouvées"] = function(){};
+  E.showMenu(menu);
+}
+
+function startRecord(force) {
+  if (!force) {
+    // check for existing file
+    var f = require("Storage").open(getFileName(fileNumber), "r");
+    if (f.readLine()!==undefined)
+      return E.showPrompt(/*LANG*/"Écraser Log "+fileNumber+"?").then(ok=>{
+        if (ok) startRecord(true); else showMenu();
+      });
+  }
+  // display
+  g.clear(1);
+  Bangle.drawWidgets();
+
+  var Layout = require("Layout");
+  var layout = new Layout({ type: "v", c: [
+      {type:"txt", font:"6x8", label:/*LANG*/"Samples", pad:2},
+      {type:"txt", id:"samples", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
+      {type:"txt", font:"6x8", label:/*LANG*/"Time", pad:2},
+      {type:"txt", id:"time", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
+      {type:"txt", font:"6x8:2", label:/*LANG*/"RECORDING", bgCol:"#f00", pad:5, fillx:1},
+    ]
+  },{btns:[ // Buttons...
+    {label:/*LANG*/"STOP", cb:()=>{
+      Bangle.removeListener('accel', accelHandler);
+      showMenu();
+    }}
+  ]});
+  layout.render();
+
+  // now start writing
+  var f = require("Storage").open(getFileName(fileNumber), "w");
+  f.write("Time (ms),AX,AY,AZ,MX,MY,MZ\n");
+  var start = getTime();
+  var sampleCount = 0;
+
+  function accelHandler(accel) {
+	Bangle.setCompassPower(1);
+    var comp = Bangle.getCompass();
+    var t = getTime()-start;
+    f.write([
+      t*1000,
+      accel.x,
+      accel.y,
+      accel.z,
+      comp.x,
+      comp.y,
+      comp.z].map(n=>Math.round(n*100000000)/100000000).join(",")+"\n");
+
+    sampleCount++;
+    layout.samples.label = sampleCount;
+    layout.time.label = Math.round(t)+"s";
+    layout.render(layout.samples);
+    layout.render(layout.time);
+  }
+
+  Bangle.setPollInterval(80); // 12.5 Hz - the default
+  Bangle.on('accel', accelHandler);
+}
+
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+showMenu();
